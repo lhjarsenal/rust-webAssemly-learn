c8aef418121726119cacff1f066268bd3ab3c483
lillard <lillard.li@kupogroup.com>
2021-12-20 17:03:40 +0800

    test and debug

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,13 @@ use wasm_bindgen::prelude::*;
 //     alert("Hello, wasm-gamo-of-life!");
 // }
 
+extern crate web_sys;
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+      web_sys::console::log_1(&format!( $( $t)* ).into());
+    }
+}
+
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, PartialOrd, PartialEq)]
@@ -35,7 +42,19 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..height * self.width).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn new() -> Universe {
+        utils::set_panic_hook();//添加钩子函数,用于调试
+
         let width = 64;
         let height = 64;
         let cells = (0..width * height).map(|i| {
@@ -88,6 +107,16 @@ impl Universe {
                 let idx = self.get_index(row, col);
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
+
+                //变化之前打印一下情况
+                log!(
+                    "cell [{},{}] is initally {:?} and has {} live neighbors",
+                    row,
+                    col,
+                    cell,
+                    live_neighbors
+                );
+
                 let next_cell = match (cell, live_neighbors) {
                     (Cell::Alive, x) if x < 2 => Cell::Dead,
                     (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
@@ -96,11 +125,28 @@ impl Universe {
                     (otherwise, _) => otherwise,
                 };
 
+                //变化之后打印一下情况
+                log!(
+                    "it becomes {:?}", next_cell
+                );
+
                 next[idx] = next_cell;
             }
         }
 
         self.cells = next;
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 }
 
@@ -108,22 +154,33 @@ use std::fmt;
 use wasm_bindgen::__rt::core::fmt::Formatter;
 
 impl fmt::Display for Universe {
-
-
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize){
-            for &cell in line{
-                let symbol = if cell == Cell::Dead{
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead {
                     "□"
-                }else{
+                } else {
                     "■"
                 };
 
-                write!(f,"{}",symbol)?;
+                write!(f, "{}", symbol)?;
             }
-            write!(f,"\n")?
+            write!(f, "\n")?
         }
         Ok(())
     }
+}
 
+//没有 #[wasm_bindgen]
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
